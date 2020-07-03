c6e95084b79c571d1acbbd1779f6fb3d26be0609
Kaustubh Dwivedi <kaustubh.dwi@gmail.com>
2020-07-03 22:46:50 +0530

    first incomplete component named menu

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-
+import Menu from './components/MenuComponent';
 function App() {
   return (
-    <div className="App">
+    <div>
      <Navbar dark color='primary'>
        <div className='container'>
          <NavbarBrand href='/'>Restorite Confusion</NavbarBrand>
        </div>
      </Navbar>
+     <Menu />
     </div>
   );
 }
