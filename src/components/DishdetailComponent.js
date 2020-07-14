90caa984c0931dff618498114d740bf5c83e883b
Kaustubh Dwivedi <kaustubh.dwi@gmail.com>
2020-07-14 17:32:18 +0530

    added header and footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -8,10 +8,10 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
        
             return(
                 <Card>
-                     <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                      <CardBody>
-                           <CardTitle> {this.props.dish.name}</CardTitle>
-                           <CardText>{this.props.dish.description}</CardText>
+                           <CardTitle> {dish.name}</CardTitle>
+                           <CardText>{dish.description}</CardText>
                      </CardBody>
                 </Card>
             );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Header from './HeaderComponent';
 import DishDetail from './DishdetailComponent';
+import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 
@@ -20,14 +21,12 @@ class Main extends Component {
 
   render() {
   return (
+  
     <div>
-     <Navbar dark color='primary'>
-       <div className='container'>
-         <NavbarBrand href='/'>Restorite Confusion</NavbarBrand>
-       </div>
-     </Navbar>
+     <Header />
      <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
      <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+      <Footer />
     </div>
   );
 }
