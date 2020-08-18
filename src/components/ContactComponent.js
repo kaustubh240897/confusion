e34db2c072bc54d29f83d8d893885a6fcb0d090e
Kaustubh Dwivedi <kaustubh.dwi@gmail.com>
2020-08-18 16:20:03 +0530

    react fetch data from server and handling errors

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col,Row, FormFeedback } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button, FormGroup, Label, Input, Col,Row, FormFeedback } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form,Errors, actions } from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -24,6 +24,7 @@ class Contact extends Component{
     handleSubmit(values){
         console.log("Current State is :" + JSON.stringify(values));
         alert("Current State is :" + JSON.stringify(values));
+        this.props.resetFeedbackForm();
        
     }
    
@@ -74,7 +75,7 @@ render() {
                 </div>
                 <div className="col-12 col-md-9">
                     
-                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Form model='feedback' onSubmit={(values) => this.handleSubmit(values)}>
                         <Row className= 'form-group'>
                             <Label htmlFor="firstname" md={2}>First Name</Label>
                             <Col md={10}>
@@ -191,7 +192,7 @@ render() {
                             </Col>
                         </Row>
                         
-                    </LocalForm>
+                    </Form>
                 </div>
             </div>
         </div>
