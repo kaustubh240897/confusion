2ff90523730fbcd9a97daef0688b7883a7a34269
Kaustubh Dwivedi <kaustubh.dwi@gmail.com>
2020-08-22 01:43:03 +0530

    react animations components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,32 +5,45 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
     
  function RenderDish({dish}) {
        
             return(
-                <Card>
-                     <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-                     <CardBody>
-                           <CardTitle> {dish.name}</CardTitle>
-                           <CardText>{dish.description}</CardText>
-                     </CardBody>
-                </Card>
-            );
+                 <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                        <Card>
+                            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle> {dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                </FadeTransform>
+                    );
 
         
        
     }
 function RenderComments({comments, postComment, dishId}){
+    
     var commentsList = comments.map(comment => {
         return( 
+             <Stagger in>
+                  
             <li key={comment.id}>
+                 <Fade in>
                 {comment.comment}
                 <br/> <br/>
                 -- {comment.author}, { new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date( Date.parse(comment.date)))}
                 <br/> <br/>
+                </Fade>
             </li>
+            
+            </Stagger>
 
         );
     });
